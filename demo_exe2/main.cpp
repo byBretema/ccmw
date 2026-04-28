dfbea32cef91aaebd580b9826fea9bb704ab10e0
ByBretema <bybretema@gmail.com>
2026-04-28 17:27:33 +0200

    python binary

--- demo_exe2/main.cpp
+++ demo_exe2/main.cpp
@@ -3,11 +3,13 @@
 #include <demo_lib1.hpp>
 #include <demo_lib2.hpp>
 #include <demo_lib3.hpp>
+#include <demo_lib4.hpp>
 
 int main() {
     std::cout << "Hello from demo2!\n";
     hello_demo_lib1();
     hello_demo_lib2();
     hello_demo_lib3();
+    hello_demo_lib4();
     return 0;
 }
---
+++ demo_lib4/demo_lib4.cpp
@@ -0,0 +1,4 @@
+#include "demo_lib4.hpp"
+#include <iostream>
+
+void hello_demo_lib4() { std::cout << "Hello from: demo_lib4!\n"; }
